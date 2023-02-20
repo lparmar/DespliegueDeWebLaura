bf5338d3ca56685e9bd005ebbc55963aa1c96421
lauraParra <laura3cp@hotmail.com>
2023-02-20 19:03:45 +0000

    prueba de rama

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 include('funciones.php');
 cabecera('Consultas Preparadas');
 echo "<h1>CONTROL DE ASISTENCIA EN D.A.W etiqueta 2.0.</h1>";
+echo "<h1>PruebaRama1</h1>";
 echo "<img src='tablas.jpg'>"; 
 		echo "</div>";
 		pie();
